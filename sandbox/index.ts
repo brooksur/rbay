59a280d1bcbe383a2e9fe3f8b1e4ef8a38e03dca
Brooks <brooks@pdrutah.com>
2023-02-03 12:15:20 -0700

    rbay

---
+++ sandbox/index.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { client } from '../src/services/redis';
+
+const run = async () => {
+	await client.hSet('car', {
+		color: 'red',
+		year: 1950,
+		engine: { cylinders: 8 },
+		owner: '',
+		service: ''
+	});
+};
+run();
