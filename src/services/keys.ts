f7a2c16c24a629164d58592ec6dab792c697d8b5
Brooks <brooks@pdrutah.com>
2023-02-13 12:37:03 -0700

    adding sets

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -2,3 +2,5 @@ export const pageCacheKey = (id: string) => `pagecache#${id}`
 export const usersKey = (id: string) => `users#${id}`
 export const sessionsKey = (id: string) => `sessions#${id}`
 export const itemsKey = (id: string) => `items#${id}`
+export const usernamesUniqueKey = () => 'usernames:unique'
+export const userLikesKey = (userId: string) => `user:likes#${userId}`
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,7 +1,7 @@
 import type { CreateUserAttrs } from '$services/types'
 import { genId } from '$services/utils'
 import { client } from '$services/redis'
-import { usersKey } from '$services/keys'
+import { usersKey, usernamesUniqueKey } from '$services/keys'
 
 export const getUserByUsername = async (username: string) => {}
 
@@ -12,7 +12,22 @@ export const getUserById = async (id: string) => {
 
 export const createUser = async (attrs: CreateUserAttrs) => {
 	const id = genId()
-	client.hSet(usersKey(id), serialiize(attrs))
+
+	// see if username is in username set
+	const exists = await client.sIsMember(usernamesUniqueKey(), attrs.username)
+
+	console.log('hello', exists)
+
+	if (exists) {
+		throw new Error('Username is taken')
+	}
+
+	// create user and add username to set
+	await Promise.all([
+		client.hSet(usersKey(id), serialiize(attrs)),
+		client.sAdd(usernamesUniqueKey(), attrs.username)
+	])
+
 	return id
 }
 
