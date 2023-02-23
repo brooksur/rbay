be53e742350fa37391d7a34cc8ccbd553d9d4194
Brooks <brooks@pdrutah.com>
2023-02-23 12:57:02 -0700

    relational data

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,1 +1,27 @@
-export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {};
+import { client } from '$services/redis'
+import { itemsKey, itemsByViewKey } from '$services/keys'
+import { deserialize } from './deserialize'
+
+export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+	const zip = (prop: string) => `${itemsKey('*')}->${prop}`
+
+	let results: any = await client.sort(itemsByViewKey(), {
+		GET: ['#', zip('name'), zip('views'), zip('endingAt'), zip('imageUrl'), zip('price')],
+		BY: 'score',
+		DIRECTION: order,
+		LIMIT: {
+			offset,
+			count
+		}
+	})
+
+	const items = []
+
+	while (results.length) {
+		const [id, name, views, endingAt, imageUrl, price, ...rest] = results
+		items.push(deserialize(id, { name, views, endingAt, imageUrl, price }))
+		results = rest
+	}
+
+	return items
+}
